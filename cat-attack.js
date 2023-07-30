a6b8c487fca12be4d50b81a7a8385c661e519ecf
Hannah Mills <millsh56@gmail.com>
2023-07-30 10:56:07 -0400

    contiuing button styling and functionality

--- cat-attack.js
+++ cat-attack.js
@@ -1,1 +1,5 @@
-document.getElementById("demo").onclick
+document.getElementById("Instructions").onclick
+
+function instructionsAlert() {
+    alert("Dear Player, I was once trapped in here like you. If you follow my advice you should make it out just fine. Be cautious, these cats mean business. When each cat meows click it to give it a treat. This will calm it down... Until it get's hungry again. Lookout for Hangry cats. Once they turn red and start to grow you don't have a lot of time before they lunge. If the cats get you three times before sunrise you might not live to see another day outside this place. I wish you better luck than me, it looks like this will be my last time at the cat cafe, at least for a long time. Good Luck! Signed Janitor Jimothy");
+  }
