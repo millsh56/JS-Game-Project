dcb2dbd457b5372d57f21ef25c0186a9efcf1daf
Hannah Mills <millsh56@gmail.com>
2023-08-01 19:15:59 -0400

    fixed background styling and continuing editing game logic in js

--- cat-attack.js
+++ cat-attack.js
@@ -1,58 +1,84 @@
+//   add cats
+  function addCat(url){
+    let image = document.createElement('cat-img')
+    image.src = url
+    image.style.position = 'absolute'
+    document.body.append(image)
+    return image
+}
+
+function place(image) {
+    image.style.position = 'fixed'
+
+    function placeCat(left, bottom) {
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+
+    return {
+        where: placeCat
+    }
+}
+
+// position cats
+place(addCat('https://placekitten.com/150/240')).where(200, 450)
+place(addCat('https://placekitten.com/150/240')).where(350, 250)
+place(addCat('https://placekitten.com/150/240')).where(450, 350)
+
+// grab instructions button from dom and wait for click
 document.getElementById("Instructions").onclick
 
+// alert function ideally on click instruction alert will pop up
 function instructionsAlert() {
     alert("Dear Player, I was once trapped in here like you. If you follow my advice you should make it out just fine. Be cautious, these cats mean business. When each cat meows click it to give it a treat. This will calm it down... Until it get's hungry again. Lookout for Hangry cats. Once they turn red and start to grow you don't have a lot of time before they lunge. If the cats get you three times before sunrise you might not live to see another day outside this place. I wish you better luck than me, it looks like this will be my last time at the cat cafe, at least for a long time. Good Luck! Signed Janitor Jimothy");
   }
 
-  document.getElementById("Play").onclick
 
-// defines cats??? do i need???
-const cats = ['freddie', 'baby', 'aminita'];
+// grab play button from dom and wait for click
+document.getElementById("Play").onclick
 
-// Function to get a random cat from the three cats
+
+// Function to get a random cat from the three cats (ie what cat will bubble be positioned over)
 function startGame (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Function to pop up the meow bubble randomly on one of the three cats
 function showMeow() {
-    const randomIndex = getRandomNumber(0, 3);
-    const meow = items[randomIndex];
+    const randomMeow = startGame(0, 3);
+    const whenMeow = items[randomIndex];
     const meow = document.getElementById('meow-bubble');
     meow.style.left = ;
     meow.style.top = ;
     object.style.display = 'block'; 
 }
-// function to 
 
-  // Get the object element from the DOM
-const freddie = document.getElementById('freddie');
-
-// Function to get a random number within a range
+// repeat of random number function 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+// functions call started info
+setInterval(() => {
+    meowBubble();
+    setTimeout(() => {
+        meow();
+    }, 1000); // time in milliseconds meow would stay on screen...???
+    hangry();
+}, 2000); // time in milliseconds for how often meow pops up
 
-// Function to randomly position the object on the screen
-function freddiePosition() {
-    freddie.style.left = ;
-    freddie.style.top = ;
-    freddie.style.display = 'block';
-}
 
-// function to make cat turn red and grow
+// function to make cat turn red and grow....
 function hangry() {
     object.style.display = 'SOMETHING';
 }
 
-// Call the functions to make the object appear and disappear at random intervals
-setInterval(() => {
-    freddie();
-    setTimeout(() => {
-        meow();
-    }, 1000); // Change the duration (in milliseconds) to control how long the object stays visible
-    hangry();
-}, 2000); // Change the interval (in milliseconds) to control how frequently the object appears
+// function for cat to lunge
 
 
+// function for adding strikes??
 
+// function for you win
+
+// function for you loose
+
+// 
