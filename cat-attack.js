c94ac57ae4cbe085c791765aa801ace333019db7
Hannah Mills <millsh56@gmail.com>
2023-08-03 23:52:15 -0400

    major changes in positioning. sadly had a hard time working on one thing at a time thus the large ammount of change in one push. good learing experince thought :)

--- cat-attack.js
+++ cat-attack.js
@@ -1,95 +1,40 @@
-// add cats
-  function addImg(url){
-    let image = document.createElement('img')
-    image.src = url
-    image.style.position = 'absolute'
-    document.body.append(image)
-    return image
-}
-
-function place(image) {
-    image.style.position = 'fixed'
-
-    function placeImg(left, bottom) {
-        image.style.left = left + 'px'
-        image.style.bottom = bottom + 'px'
-    }
-
-    return {
-        where: placeImg
-    }
-}
-
-// position cats
-place(addImg('https://placekitten.com/150/240')).where(200, 100)
-place(addImg('https://placekitten.com/150/240')).where(765, 100)
-place(addImg('https://placekitten.com/150/240')).where(1320, 100)
-
-
 // alert function ideally on click instruction alert will pop up
 function instructionsAlert() {
     alert("Dear Player,\nI was once trapped in here like you. If you follow my advice you should make it out just fine.\nBe cautious, these cats mean business. When each cat meows click it to give it a treat. This will calm it down... Until it get's hungry again. Lookout for Hangry cats. Once they turn red and start to grow you don't have a lot of time before they lunge.\nIf the cats get you three times before sunrise you might not live to see another day outside this place. I wish you better luck than me, it looks like this will be my last time at the cat cafe, at least for a long time.\nGood Luck! Signed Janitor Jimothy");
-  }
+}
 
-  // grab instructions button from dom 
+// grab instructions button from dom 
 
-  const instructionsButton = document.getElementById("Instructions")
+const instructionsButton = document.getElementById("Instructions")
 
 // const instructionsButton = document.getElementById("Instructions")
 instructionsButton.addEventListener('click', e => {
     instructionsAlert()
 })
 
-// meow bubble code
-function placeMeowBubble(image) {
-    const meow = document.createElement("div");
-    
-    // Set the word bubble content based on the game logic
-    // For example, you can use textContent to set the words inside the bubble
-    wordBubble.textContent = "Hello!";
-    // Position the word bubble over the image
-    // For example, you can use image.getBoundingClientRect() to get the image position
-    // and set the word bubble's left and top position accordingly
-    // Example: wordBubble.style.left = `${imagePosition.left}px`;
-    //          wordBubble.style.top = `${imagePosition.top}px`;
-    document.body.append(wordBubble);
-
-    // Wait for 3 seconds before turning the image red and growing it
-    setTimeout(() => {
-        image.style.filter = "hue-rotate(90deg)";
-        image.style.transform = "scale(4)";
-    }, 3000);
-
-    // Wait for 7 seconds before animating the image to fill the screen
-    setTimeout(() => {
-        // Start the animation to grow the image to fill the screen
-        // For example, you can use CSS transitions to smoothly animate the image size
-        // Example: image.style.transition = "all 5s ease";
-        //          image.style.transform = "scale(5)";
-        // After the animation, remove the word bubble from the DOM
-        wordBubble.remove();
-    }, 7000);
-}
 
 // start game function
-function startGame() {
+function meowBubbleMovement() {
 
     // grab images and get them to listen for clicks
-    const allCats = document.querySelectorAll(".game-image");
+    const allCats = document.querySelectorAll(".cats");
     allCats.forEach((image) => {
         image.style.opacity = 1;
         image.addEventListener("click", handleImageClick);
     });
 
-    const meow = document.getElementById('meow-bubble');
-    // meow.style.left = ;
-    // meow.style.top = ;
+    const meowBubble = document.getElementById('meow-bubble');
     object.style.display = 'block';
+    image.getBoundingClientRect()
+    meowBubble.style.left = `${imagePosition.left}px`;
+    meowBubble.style.top = `${imagePosition.top}px`;
+    document.body.append(meowBubble);
 
-    // Show the word bubble over a random image to start the game
-    const meowBubblePosition = getRandomNumber(0, allCats.length - 1);
-    showWordBubble(allCats[meowBubblePosition]);
+    // Show the meow bubble over a random image to start the game
+    const meowBubblePosition = Math.floor(Math.random() * (max - min + 1) + min);
+    meowBubble(allCats[meowBubblePosition]);
 }
+
 
 // Add a click event listener to the play button
 const playButton = document.getElementById("playButton");
@@ -103,31 +48,9 @@ function startGameTimer() {
         if (timeLeft <= 0) {
             youWin();
         }
-    }, 1000); 
-}
-// Function to get a random cat from the three cats (ie what cat will bubble be positioned over)
-function startGame (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    }, 1000);
 }
 
-// Function to pop up the meow bubble randomly on one of the three cats
-function showMeow() {
-    const play = startGame(0, 3);
-    const start = items[randomIndex];
-    const meow = document.getElementById('meow-bubble');
-    // meow.style.left = ;
-    // meow.style.top = ;
-    object.style.display = 'block'; 
-}
-
-// repeat of random number function 
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-// this is what will start when play is clicked
-
-// const timerInterval = "hello"
 const timeLeft = 5 * 60;
 
 // Function to update the timer display
@@ -136,6 +59,7 @@ function updateTimer() {
     const seconds = timeLeft % 60;
     const timer = document.getElementById("timer");
     timer.textContent = `Time until morning: ${minutes}:${seconds.toString().padStart(2, "0")}`;
+    document.append("timer")
 }
 
 // function for winning condition
@@ -143,6 +67,50 @@ function youWin() {
     clearInterval(timerInterval);
     alert("You survived the night!\nThe cats thank you for the treats!\nCome back anytime you like! :3");
 }
+
+animateImage();
+
+// function to check for conditions for cat to lunge
+function lungePath(event) {
+    const image = event.target;
+
+    if (image.style.transform === "scale(4)") {
+        // Image has reached full size, show word bubble over a different random image
+        const allImages = document.querySelectorAll(".game-image");
+        const randomCat = getRandomNumber(0, allImages.length - 1);
+        meowBubbleMovement(allImages[randomCat]);
+    }
+    else {
+        // Image is still growing, change its color to red and grow it further
+        image.style.filter = "hue-rotate(90deg)";
+        image.style.transform = "scale(4)";
+    }
+}
+
+// function for adding strikes
+const strikes = 0;
+
+function whenCatLunges() {
+    strikes++;
+    const strikesObject = document.getElementById("strikes");
+    strikesObject.textContent = `Strikes: ${strikes}`;
+
+    // see if there are three strikes
+    if (strikes === 3) {
+        // Perform actions when the player loses
+        alert("GAME OVER!\nThe cats got you three times so you are dinner!\nHit play to try again");
+    }
+}
+
+
+
+// // Function to get a random cat from the three cats (ie what cat will bubble be positioned over)
+// function startGame (min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// this is what will start when play is clicked
+
 
 // // functions call started info
 // setInterval(() => {
@@ -162,28 +130,7 @@ function youWin() {
 //     setTimeout(() => {
 //         image.style.filter = "hue-rotate(90deg)";
 //         image.style.transform = "scale(1.5)";
-//     }, 5000); 
+//     }, 5000);
 // }
 
-animateImage();
 
-// function for cat to lunge
-function lunge() {
-
-}
-
-
-// function for adding strikes
-const strikes = 0; 
-
-function whenCatLunges() {
-    strikes++;
-    const strikesObject = document.getElementById("strikes");
-    strikesObject.textContent = `Strikes: ${strikes}`;
-
-    // see if there are three strikes
-    if (strikes === 3) {
-        // Perform actions when the player loses
-        alert("GAME OVER!\nThe cats got you three times so you are dinner!\nHit play to try again");
-    }
-}
