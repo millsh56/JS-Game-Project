7c93e9719c885dc0a3d47391dbc5cf5896a0edde
Hannah Mills <millsh56@gmail.com>
2023-08-04 22:46:48 -0400

    refactored js code and condensed game logic... its broken but its getting there

--- cat-attack.js
+++ cat-attack.js
@@ -1,136 +1,139 @@
-// alert function ideally on click instruction alert will pop up
+
+// Alert function - display instructions when the "Instructions" button is clicked
 function instructionsAlert() {
-    alert("Dear Player,\nI was once trapped in here like you. If you follow my advice you should make it out just fine.\nBe cautious, these cats mean business. When each cat meows click it to give it a treat. This will calm it down... Until it get's hungry again. Lookout for Hangry cats. Once they turn red and start to grow you don't have a lot of time before they lunge.\nIf the cats get you three times before sunrise you might not live to see another day outside this place. I wish you better luck than me, it looks like this will be my last time at the cat cafe, at least for a long time.\nGood Luck! Signed Janitor Jimothy");
+    // Your alert message here
+    alert("Dear Player,\nI was once trapped in here like you. If you follow my advice you should make it out just fine.\nBe cautious, these cats mean business. When each cat meows click it to give it a treat. This will calm it down... Until it gets hungry again. Lookout for Hangry cats. Once they turn red and start to grow you don't have a lot of time before they lunge.\nIf the cats get you three times before sunrise you might not live to see another day outside this place. I wish you better luck than me, it looks like this will be my last time at the cat cafe, at least for a long time.\nGood Luck! Signed Janitor Jimothy");
 }
 
-// grab instructions button from dom 
-
+// Event listener for the "Instructions" button to trigger the instructionsAlert function
 const instructionsButton = document.getElementById("Instructions")
-
-// const instructionsButton = document.getElementById("Instructions")
 instructionsButton.addEventListener('click', e => {
     instructionsAlert()
 })
 
+// Function to create and append the meow bubble image to the page
+function createMeowBubble() {
+    const meowBubble = document.createElement("img");
+    meowBubble.id = "meow-bubble";
+    meowBubble.src = "assets/meow.png"; // Replace with the path of your meow bubble image
+    meowBubble.style.display = "none";
+    document.getElementById("gameBoard").appendChild(meowBubble);
+    return meowBubble;
+}
 
-// start game function
-function meowBubbleMovement() {
+// Function to start the game when the "Play" button is clicked
+function startGame() {
+    const timeLeft = 5 * 60;
+    let timerInterval;
+    let strikes = 0;
 
-    // grab images and get them to listen for clicks
+    // Function to update the timer display
+    function updateTimerDisplay(minutes, seconds) {
+        const timerDisplay = document.createElement("p");
+        timerDisplay.id = "timerDisplay";
+        timerDisplay.textContent = `Time until morning: ${minutes}:${seconds.toString().padStart(2, "0")}`;
+
+        // Append the timer display after the "Time Until Morning" paragraph
+        const timeUntilMorningParagraph = document.getElementById("timer");
+        timeUntilMorningParagraph.insertAdjacentElement('afterend', timerDisplay);
+    }
+
+    // Function to start the game timer
+    function startGameTimer() {
+        timerInterval = setInterval(() => {
+            timeLeft--;
+            if (timeLeft <= 0) {
+                timeLeft = 0;
+                clearInterval(timerInterval);
+                youWin();
+            }
+            const minutes = Math.floor(timeLeft / 60);
+            const seconds = timeLeft % 60;
+            updateTimerDisplay(minutes, seconds);
+        }, 1000);
+    }
+
+    // Function to display the "You Win" alert when the game ends
+    function youWin() {
+        clearInterval(timerInterval);
+        alert("You survived the night!\nThe cats thank you for the treats!\nCome back anytime you like! :3");
+    }
+
+    // Function to handle when a cat is clicked
+    function handleCatClick() {
+        // Check if the cat is in the lunge animation state
+        if (this.style.transform === "scale(100)") {
+            // Lunge animation complete, show meow bubble over a different random cat
+            showMeowBubble();
+        } else {
+            // Cat was not clicked during lunge animation, increase strikes and update display
+            strikes++;
+            const strikesObject = document.getElementById("strikes");
+            strikesObject.textContent = `Strikes: ${strikes}`;
+
+            // Check if there are three strikes
+            if (strikes === 3) {
+                // Perform actions when the player loses
+                clearInterval(timerInterval);
+                alert("GAME OVER!\nThe cats got you three times so you are dinner!\nHit play to try again");
+            }
+        }
+    }
+
+    // Start the game timer
+    startGameTimer();
+
+    // Grab cats and wait for clicks
     const allCats = document.querySelectorAll(".cats");
-    allCats.forEach((image) => {
-        image.style.opacity = 1;
-        image.addEventListener("click", handleImageClick);
+    allCats.forEach((cat) => {
+        cat.style.opacity = 1; // Show the cat
+        cat.addEventListener("click", handleCatClick);
     });
 
-    const meowBubble = document.getElementById('meow-bubble');
-    object.style.display = 'block';
-    image.getBoundingClientRect()
-    meowBubble.style.left = `${imagePosition.left}px`;
-    meowBubble.style.top = `${imagePosition.top}px`;
-    document.body.append(meowBubble);
+    // Show meow bubble over random cat
+    function showMeowBubble() {
+        const randomCatAttack = getRandomNumber(0, allCats.length - 1);
+        const hangryCat = allCats[randomCatAttack];
 
-    // Show the meow bubble over a random image to start the game
-    const meowBubblePosition = Math.floor(Math.random() * (max - min + 1) + min);
-    meowBubble(allCats[meowBubblePosition]);
+        const meowBubble = createMeowBubble();
+        meowBubble.style.display = "block";
+
+        // Positioning for meow bubble
+        const catPosition = hangryCat.getBoundingClientRect();
+        meowBubble.style.left = `${catPosition.left}px`;
+        meowBubble.style.top = `${catPosition.top}px`;
+
+        // Wait for cat to be clicked
+        setTimeout(() => {
+            // If not clicked, animate the cat
+            if (hangryCat.style.transform !== "scale(4)") {
+                hangryCat.style.filter = "hue-rotate(90deg)";
+                hangryCat.style.transform = "scale(4)";
+
+                // Wait again before lunge if not clicked
+                setTimeout(() => {
+                    const newCatImage = hangryCat.dataset.newImage; // Get the data-new-image attribute value
+                    hangryCat.src = newCatImage;
+                    hangryCat.style.transition = "transform 0.3s ease";
+
+                    // Lunge
+                    setTimeout(() => {
+                        hangryCat.style.transform = "scale(100)";
+                    }, 10);
+                }, 2000);
+            }
+        }, 5000);
+    }
+
+    // Initial call to show meow bubble
+    showMeowBubble();
 }
 
-
-// Add a click event listener to the play button
-const playButton = document.getElementById("playButton");
+// Event listener for the "Play" button to start the game
+const playButton = document.getElementById("Play");
 playButton.addEventListener("click", startGame);
-// Start the timer
-function startGameTimer() {
-    timerInterval = setInterval(() => {
-        timeLeft--;
-        updateTimer();
 
-        if (timeLeft <= 0) {
-            youWin();
-        }
-    }, 1000);
+// Function to get a random number within a range (used for random cat selection)
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-const timeLeft = 5 * 60;
-
-// Function to update the timer display
-function updateTimer() {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-    const timer = document.getElementById("timer");
-    timer.textContent = `Time until morning: ${minutes}:${seconds.toString().padStart(2, "0")}`;
-    document.append("timer")
-}
-
-// function for winning condition
-function youWin() {
-    clearInterval(timerInterval);
-    alert("You survived the night!\nThe cats thank you for the treats!\nCome back anytime you like! :3");
-}
-
-animateImage();
-
-// function to check for conditions for cat to lunge
-function lungePath(event) {
-    const image = event.target;
-
-    if (image.style.transform === "scale(4)") {
-        // Image has reached full size, show word bubble over a different random image
-        const allImages = document.querySelectorAll(".game-image");
-        const randomCat = getRandomNumber(0, allImages.length - 1);
-        meowBubbleMovement(allImages[randomCat]);
-    }
-    else {
-        // Image is still growing, change its color to red and grow it further
-        image.style.filter = "hue-rotate(90deg)";
-        image.style.transform = "scale(4)";
-    }
-}
-
-// function for adding strikes
-const strikes = 0;
-
-function whenCatLunges() {
-    strikes++;
-    const strikesObject = document.getElementById("strikes");
-    strikesObject.textContent = `Strikes: ${strikes}`;
-
-    // see if there are three strikes
-    if (strikes === 3) {
-        // Perform actions when the player loses
-        alert("GAME OVER!\nThe cats got you three times so you are dinner!\nHit play to try again");
-    }
-}
-
-
-
-// // Function to get a random cat from the three cats (ie what cat will bubble be positioned over)
-// function startGame (min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-
-// this is what will start when play is clicked
-
-
-// // functions call started info
-// setInterval(() => {
-//     meowBubble();
-//     setTimeout(() => {
-//         meow();
-//     }, 1000); // time in milliseconds meow would stay on screen...???
-//     hangry();
-// }, 2000); // time in milliseconds for how often meow pops up
-
-
-// function to make cat turn red and grow....
-
-// async function hangryCat() {
-//     await
-//     image.style.opacity = 1;
-//     setTimeout(() => {
-//         image.style.filter = "hue-rotate(90deg)";
-//         image.style.transform = "scale(1.5)";
-//     }, 5000);
-// }
-
-
